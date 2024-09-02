c2f9c58bb3ee4feb16137e9c51b3774545d9b784
Dmitry Zotov <dmzt08@gmail.com>
2024-09-02 16:30:23 +0500

    added support for DID Parameters

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,14 +1,48 @@
-import {DIDResolutionResult, DIDResolver, DIDDocument, DIDResolutionMetadata, DIDDocumentMetadata} from "did-resolver"
+import {
+  DIDResolutionResult,
+  DIDResolver,
+  DIDDocument,
+  DIDResolutionMetadata,
+  DIDDocumentMetadata,
+  ParsedDID,
+} from "did-resolver"
 
 export function getResolver(url: string): Record<string, DIDResolver> {
-  async function resolve(did: string): Promise<DIDResolutionResult> {
+  async function resolve(
+    did: string,
+    parsed: ParsedDID,
+  ): Promise<DIDResolutionResult> {
+    let didUri = did
     let didDocument: DIDDocument | null = null
     let didDocumentMetadata: DIDDocumentMetadata = {}
     let didResolutionMetadata: DIDResolutionMetadata = {}
 
+    if (parsed.query) {
+      // verify query parameters
+      // see https://www.w3.org/TR/did-core/#did-parameters
+      const allowedQueryParams: string[] = [
+        "service",
+        "relativeRef",
+        "versionId",
+        "versionTime",
+        "hl",
+      ]
+      const searchParams = new URLSearchParams(parsed.query)
+      for (const key of searchParams.keys()) {
+        if (!allowedQueryParams.includes(key)) {
+          return getResolutionError(
+            "invalidDidUrl",
+            `Invalid DID URL query parameter: ${key}`,
+          )
+        }
+      }
+
+      didUri = `${did}?${parsed.query}`
+    }
+
     try {
-      const response = await fetch(`${url}/1.0/identifiers/${did}`)
-      const result = await response.json() as DIDResolutionResult
+      const response = await fetch(`${url}/1.0/identifiers/${didUri}`)
+      const result = (await response.json()) as DIDResolutionResult
       didDocument = result.didDocument
       didDocumentMetadata = result.didDocumentMetadata
       didResolutionMetadata = result.didResolutionMetadata
